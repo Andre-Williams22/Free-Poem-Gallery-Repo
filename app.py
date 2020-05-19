3b6f9de4bcb7b8ab921faa12cb1ab964e4d1c70c
Andre Williams <awjrs22@gmail.com>
2020-05-18 22:05:46 -0700

    updates backend

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for
 
 
 
-
 app = Flask(__name__)
-
 
 
 @app.route('/')
@@ -23,3 +21,4 @@ def home_two():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
