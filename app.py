ee3697ed8b94c4e305113b5b0994111e18c25ced
Andre Williams <awjrs22@gmail.com>
2020-05-22 21:08:33 -0700

    adds git and post methods

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html')
 
     
-@app.route('/poem')
+@app.route('/poem', methods=['GET', 'POST'])
 def poem():
     return render_template('poems.html')
 
