45b8c6e68ebc528749449062f462e24d10fc3eac
Andre Williams <awjrs22@gmail.com>
2020-05-22 21:24:32 -0700

    fixes heroku error?

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('About.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('Contact.html')
 
     
 @app.route('/poem')
 def poem():
-    return render_template('poems.html')
+    return render_template('Poems.html')
 
 
 if __name__ == '__main__':
